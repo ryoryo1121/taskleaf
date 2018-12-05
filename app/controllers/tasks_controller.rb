505c408eaa9d82515438d492d4689dee070139ca
ryoryo1121 <ry01121xx@gmail.com>
2018-12-05 22:51:19 +0900

    add set_tasks
    重複を避けるため　set_tasks を定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -12,16 +13,13 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
   end
   def destroy
-    task = current_user.tasks.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice:"タスク「#{task.name}を削除しました。」"
   end
@@ -42,4 +40,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :description)
   end
 
+  def set_tasks
+    @task = current_user.tasks.find(params[:id])
+  end
 end
