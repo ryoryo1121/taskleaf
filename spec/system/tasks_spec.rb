6427f6594879a99afab7dc22bf0fb12560b31bd9
ryoryo1121 <ry01121xx@gmail.com>
2018-12-09 00:26:26 +0900

    shared_examples
    shared_examples の利用

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -6,12 +6,14 @@ describe 'タスク管理機能', type: :system do
   let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a )}
 
   before do
-    FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-    # 作成者がユーザーAであるタスクを作成しておく
     visit login_path
     fill_in 'メールアドレス', with: login_user.email
     fill_in 'パスワード', with: login_user.password
     click_button 'ログインする'
+  end
+
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
   end
 
   describe 'タスク一覧機能' do
@@ -19,10 +21,7 @@ describe 'タスク管理機能', type: :system do
     context 'ユーザーAがログインしているとき' do
       let(:login_user){ user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -43,9 +42,7 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content'最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
